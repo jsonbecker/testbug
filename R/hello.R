b5a5913a3e27125bf6b6e7979b60ab82cc5ed6b8
jsonbecker <jason@jbecker.co>
2017-10-05 21:09:46 -0400

    Show that this bug isn't present with src_postgres

--- R/hello.R
+++ R/hello.R
@@ -2,6 +2,21 @@
 #' @importFrom dplyr tbl
 
 test_tmp_tables <- function(connection, my_data){
-  copy_to(dest = connection, df = my_data, name = 'tmp_data', overwrite = TRUE)
+  copy_to(dest = connection,
+          df = my_data,
+          name = 'tmp_data',
+          overwrite = TRUE,
+          temporary = TRUE)
+  tbl(connection, 'tmp_data')
+}
+
+#' @importFrom dplyr src_postgres
+test_dplyr_native_copy <- function(my_data){
+  connection <- dplyr::src_postgres('allovue_test', 'localhost', '5432', 'postgres')
+  copy_to(dest = connection,
+          df = my_data,
+          name = 'tmp_data',
+          overwrite = TRUE,
+          temporary = TRUE)
   tbl(connection, 'tmp_data')
 }
--- tests/testthat/test_savepoint_error.R
+++ tests/testthat/test_savepoint_error.R
@@ -20,4 +20,37 @@ test_that('This code creates a SAVEPOINT error with a large tmp table',{
   connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
   expect_silent(test_tmp_tables(connection, df))
   DBI::dbDisconnect(connection)
+
+  df <- data.frame(numbers = seq_len(1025))
+  connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
+  expect_silent(test_tmp_tables(connection, df))
+  DBI::dbDisconnect(connection)
+})
+
+
+test_that('This code does not create a SAVEPOINT error with a large tmp table',{
+  df <- data.frame(numbers = seq_len(1))
+  connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
+  expect_silent(test_tmp_tables(connection, df))
+  DBI::dbDisconnect(connection)
+
+  df <- data.frame(numbers = seq_len(1000))
+  connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
+  expect_silent(test_tmp_tables(connection, df))
+  DBI::dbDisconnect(connection)
+
+  df <- data.frame(numbers = seq_len(1024))
+  connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
+  expect_silent(test_tmp_tables(connection, df))
+  DBI::dbDisconnect(connection)
+
+  df <- data.frame(numbers = seq_len(1025))
+  connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
+  expect_silent(test_tmp_tables(connection, df))
+  DBI::dbDisconnect(connection)
+
+  df <- data.frame(numbers = seq_len(1025))
+  connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
+  expect_silent(test_tmp_tables(connection, df))
+  DBI::dbDisconnect(connection)
 })
