d46c6ab162e3d8ff81e706230df5530d8aa37401
jsonbecker <jason@jbecker.co>
2017-10-05 20:39:06 -0400

    Simplified reproducible example further

--- tests/testthat/test_savepoint_error.R
+++ tests/testthat/test_savepoint_error.R
@@ -1,11 +1,23 @@
 context('trying to trip the SAVEPOINT error')
 
 test_that('This code creates a SAVEPOINT error with a large tmp table',{
-  df <- data.frame(date = seq.Date(as.Date('2010-09-01'),
-                                   as.Date('2020-09-01'),
-                                   length.out = 25000),
-                   numbers = rnorm(1000, 200,n = 25000))
+  df <- data.frame(numbers = seq_len(1))
   connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
   expect_silent(test_tmp_tables(connection, df))
-  dbDisconnect(connection)
+  DBI::dbDisconnect(connection)
+
+  df <- data.frame(numbers = seq_len(1000))
+  connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
+  expect_silent(test_tmp_tables(connection, df))
+  DBI::dbDisconnect(connection)
+
+  df <- data.frame(numbers = seq_len(1024))
+  connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
+  expect_silent(test_tmp_tables(connection, df))
+  DBI::dbDisconnect(connection)
+
+  df <- data.frame(numbers = seq_len(1025))
+  connection <- DBI::dbConnect(odbc::odbc(), 'Allovue Test')
+  expect_silent(test_tmp_tables(connection, df))
+  DBI::dbDisconnect(connection)
 })
